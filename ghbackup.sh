22144b9077753680cb3391a046def132b0a253e8
Alex Pardoe <alex@pardoe.io>
2018-05-12 20:40:44 +0100

    Fix backup script.

--- ghbackup.sh
+++ ghbackup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 mount | grep '/ghbackup ' > /dev/null
-if [ "$?" -ne 0 ]
-then
+if [ "$?" -ne 0 ]; then
   echo 'Volume /ghbackup must be mounted'
   exit 1
 fi
@@ -10,18 +9,15 @@ fi
 if [[ -z "${GITHUB_USER}" ]]; then
   echo 'Variable GITHUB_USER must be defined'
   exit 1
-else
 fi
 
 if [[ -z "${GITHUB_SECRET}" ]]; then
   echo 'Variable GITHUB_SECRET must be defined'
   exit 1
-else
 fi
 
 ghbackup -silent -account $GITHUB_USER -secret $GITHUB_SECRET /ghbackup
-if [ "$?" -ne 0 ]
-then
+if [ "$?" -ne 0 ]; then
   echo 'An error has occured in ghbackup'
   exit 1
 fi
